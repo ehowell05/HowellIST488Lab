cc0ebf032dba8e0cf962949a8b3fb72086bc5f3e
Eddie Howell <eihowell@syr.edu>
2026-01-21 13:21:47 -0500

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Eddie Howell IST488 Labs",
+    page_icon="🥼",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.page(
+    'labs/lab1',
+    title = "Lab 1",
+    icon = "📄",
+    url_path = None,
+    default = False
+)
+
+st.page(
+    'labs/lab2',
+    title = "Lab 2",
+    icon = "🥼",
+    url_path = None,
+    default = False
 )
