6105837c45c93b47d7fda57c94c50e9be6d3ee6f
Eddie Howell <eihowell@syr.edu>
2026-01-21 13:29:40 -0500

    3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-Lab1 = st.Page('labs/lab1',
+Lab1 = st.Page('labs/lab1.py',
     title = "Lab 1",
     icon = "📄",
     url_path = None,
     default = False)
-Lab2 = st.Page('labs/lab2',
+Lab2 = st.Page('labs/lab2.py',
     title = "Lab 2",
     icon = "🥼",
     url_path = None,
