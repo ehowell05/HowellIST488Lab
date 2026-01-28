ddc8de80fe4080e3fa5d5cbc0913770caa5b1aff
Eddie Howell <eihowell@syr.edu>
2026-01-28 12:53:10 -0500

    new 1

--- labs/lab2.py
+++ labs/lab2.py
@@ -9,21 +9,19 @@ st.write(
 st.sidebar.title("Lab 2 Settings")
 add_sum_sbox = st.sidebar.selectbox("Which kind of summary would you like to use?", ["in 100 Words", "in 2 Paragraphs", "in 5 Bullet Points"])
 add_model_checkbox = st.checkbox("Would you like to use the stonger model?", value = False)
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.secrets.EddieOpenAPIKey
-#if not openai_api_key:
-    #st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-#else:
+
+
+
 def model_check(add_model_checkbox):
     if add_model_checkbox:
         model_check = "gpt-5-mini"
     else:
         model_check = "gpt-5-nano"
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+openai_api_key = st.secrets.EddieOpenAPIKey
 
-
-# Create an OpenAI client.
 client = OpenAI(api_key=openai_api_key)
 
 try:
@@ -35,13 +33,6 @@ except Exception as e:
 # Let the user upload a file via `st.file_uploader`.
 uploaded_file = st.file_uploader(
     "Upload a document (.txt or .md)", type=("txt", "md")
-)
-
-# Ask the user for a question via `st.text_area`.
-question = st.text_area(
-    "Now ask a question about the document!",
-    placeholder="Can you give me a short summary?",
-    disabled=not uploaded_file,
 )
 
 if uploaded_file and question:
@@ -57,9 +48,10 @@ if uploaded_file and question:
 
     # Generate an answer using the OpenAI API.
     stream = client.chat.completions.create(
-        model = model_check,
+        model=model_check(add_model_checkbox),
         messages=messages,
         stream=True,
     )
+
     # Stream the response to the app using `st.write_stream`.
     st.write_stream(stream)
