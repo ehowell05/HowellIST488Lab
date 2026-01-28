347a4bf15898031bb0c885511b4d060e15f98423
Eddie Howell <eihowell@syr.edu>
2026-01-28 12:49:09 -0500

    8

--- labs/lab2.py
+++ labs/lab2.py
@@ -7,7 +7,8 @@ st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
 )
 st.sidebar.title("Lab 2 Settings")
-add_model_sbox = st.sidebar.selectbox("Which kind of summary would you like to use?", ["in 100 Words", "in 2 Paragraphs", "in 5 Bullet Points"])
+add_sum_sbox = st.sidebar.selectbox("Which kind of summary would you like to use?", ["in 100 Words", "in 2 Paragraphs", "in 5 Bullet Points"])
+add_model_checkbox = st.checkbox("Would you like to use the stonger model?", value = False)
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
@@ -15,6 +16,12 @@ openai_api_key = st.secrets.EddieOpenAPIKey
 #if not openai_api_key:
     #st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 #else:
+def model_check(add_model_checkbox):
+    if add_model_checkbox:
+        model_check = "gpt-5-mini"
+    else:
+        model_check = "gpt-5-nano"
+
 
 # Create an OpenAI client.
 client = OpenAI(api_key=openai_api_key)
@@ -44,16 +51,15 @@ if uploaded_file and question:
     messages = [
         {
             "role": "user",
-            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            "content": f"Here's a document: {document} \n\n---\n\n {add_sum_sbox}",
         }
     ]
 
     # Generate an answer using the OpenAI API.
     stream = client.chat.completions.create(
-        model="gpt-5-nano",
+        model = model_check,
         messages=messages,
         stream=True,
     )
-
     # Stream the response to the app using `st.write_stream`.
     st.write_stream(stream)
