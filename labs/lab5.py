ecd4b7a7d764511f9b9c04e1bf93d564e6470ab0
Eddie Howell <eihowell@syr.edu>
2026-02-16 14:03:11 -0500

    adding space?

--- labs/lab5.py
+++ labs/lab5.py
@@ -51,11 +51,11 @@ if st.sidebar.button('Get Outfit'):
     weather = get_current_weather(location, units='imperial')
 
     prompt = f'''You are an outfit recommendation chatbot. Assume that this outfit is for a 6'4 man and give an output in this way only and do not add any more or any less.
-    Hat? Yes/No \n
-    Shirt: \n
-    Pants: \n
-    Jacket? Yes/No \n
-    Shoes: \n
+    Hat? Yes/No \n \n
+    Shirt: \n \n
+    Pants: \n \n
+    Jacket? Yes/No \n \n
+    Shoes: \n \n
     Based on the weather in {location}, suggest an appropriate outfit.
 
     Current weather:
